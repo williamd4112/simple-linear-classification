15dfa3078327782013833c98f1bdeb5cdd50d73f
williamd4112 <williamd4112@hotmail.com>
2017-04-11 01:11:24 +0800

    clean unecessary files

--- eval_generative.sh
+++
@@ -1,6 +0,0 @@
-DATASETS="data/class1.npy,data/class2.npy,data/class3.npy"
-D=2
-LOAD=$1
-python main.py --task eval --load ${LOAD}.npy --basis ${LOAD}_basis.npy --std ${LOAD}_std.npy --X $DATASETS --model gen
-
-
--- plot_discriminative.sh
+++
@@ -1,4 +0,0 @@
-DATASETS="data/class1.npy,data/class2.npy,data/class3.npy"
-LOAD=$1
-python main.py --task plot --X $DATASETS --load ${LOAD}.npy --basis ${LOAD}_basis.npy --std ${LOAD}_std.npy --model dis
-
